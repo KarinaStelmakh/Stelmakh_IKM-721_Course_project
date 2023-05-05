83f4cd297a7b6ef91b32029ae7ba884122cedd1d
Karina <stelmakhkarina23@gmail.com>
2023-05-05 14:34:30 +0300

    Finish Task number 8

--- Form1.cs
+++ Form1.cs
@@ -16,9 +16,29 @@ namespace Stelmakh_IKM_721Б_Course_project
     {
         private bool Mode; // Режим дозволу / заборони введення даних
         private MajorWork MajorObject; // Створення об'єкта класу MajorWork
+        ToolStripLabel dateLabel;
+        ToolStripLabel timeLabel;
+        ToolStripLabel infoLabel;
+        Timer timer;
         public Form1()
         {
             InitializeComponent();
+            infoLabel = new ToolStripLabel();
+            infoLabel.Text = "Поточні дата та час:";
+            dateLabel = new ToolStripLabel();
+            timeLabel = new ToolStripLabel();
+            statusStrip1.Items.Add(infoLabel);
+            statusStrip1.Items.Add(dateLabel);
+            statusStrip1.Items.Add(timeLabel);
+            timer = new Timer() { Interval = 1000 };
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            dateLabel.Text = DateTime.Now.ToLongDateString();
+
+            timeLabel.Text = DateTime.Now.ToLongTimeString();
         }
 
         private void tClock_Tick(object sender, EventArgs e)
@@ -37,6 +57,8 @@ namespace Stelmakh_IKM_721Б_Course_project
             A.tAbout.Start();
             A.ShowDialog(); // відображення діалогового вікна About 
             this.Mode = true;
+            toolTip1.SetToolTip(bSearch, "Натисніть на кнопку для пошуку"); 
+            toolTip1.IsBalloon = true;
         }
 
         private void bStart_Click(object sender, EventArgs e)
@@ -95,7 +117,9 @@ namespace Stelmakh_IKM_721Б_Course_project
         private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
         {
             About A = new About();
+            A.progressBar1.Hide();
             A.ShowDialog();
+           
         }
 
         private void зберегтиЯкToolStripMenuItem_Click(object sender, EventArgs e)
